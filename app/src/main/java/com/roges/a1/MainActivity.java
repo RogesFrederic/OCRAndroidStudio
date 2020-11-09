7d7d3dbb661568837601231e643311a676ee2ee3
Frédéric Rogès <roges.frederic@gmail.com>
2020-11-09 22:13:00 +0100

    Référencez les élements graphiques de votre activité

--- app/src/main/java/com/roges/a1/MainActivity.java
+++ app/src/main/java/com/roges/a1/MainActivity.java
@@ -3,12 +3,23 @@ package com.roges.a1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mGreetingText;
+    private EditText mNameInput;
+    private Button mPlayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGreetingText= (TextView) findViewById(R.id.activity_main_greeting_txt);
+        mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
+        mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
+
     }
 }
